8e378ca3354cc6106d14ffce5e9d718aa69778d1
Mikhail Fedosov <tbs.micle@gmail.com>
2013-05-21 22:48:41 +0400

    fix #1: 'Убирать параметры URL'

--- habra_chrome/assets/javascripts/habra_chrome.js
+++ habra_chrome/assets/javascripts/habra_chrome.js
@@ -1,4 +1,9 @@
 if (document.title.indexOf("оступ к странице ограничен") > -1)
 {
-	chrome.extension.sendRequest({ redirect: "http://webcache.googleusercontent.com/search?q=cache%3A" + window.location.href + "&aq=f&sourceid=chrome&ie=UTF-8" });
+	chrome.extension.sendRequest(
+	{
+		redirect: "http://webcache.googleusercontent.com/search?q=cache%3A" +
+		          // http://habrahabr.ru/post/180469/?utm_source=feedly -> http://habrahabr.ru/post/180469/
+		          location.href.substring(0, location.href.lastIndexOf("/") + 1) +
+		          "&aq=f&sourceid=chrome&ie=UTF-8" });
 }
