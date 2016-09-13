6a36cb25f1afb8a8a9848f18488bffe34d660824
i.izotov <i.izotov@criteo.com>
2016-09-13 16:21:09 +0200

    Correction for https and fix for no more distinctive title

--- habra_chrome/assets/javascripts/habra_chrome.js
+++ habra_chrome/assets/javascripts/habra_chrome.js
@@ -1,9 +1,11 @@
-if (document.title.indexOf("оступ к странице ограничен") > -1)
+var h1 = document.getElementById('layout').getElementsByTagName('h1')[0];
+if (h1.textContent.indexOf("Доступ к публикации закрыт") > -1)
 {
-	chrome.extension.sendRequest(
-	{
-		redirect: "http://webcache.googleusercontent.com/search?q=cache%3A" +
-		          // http://habrahabr.ru/post/180469/?utm_source=feedly -> http://habrahabr.ru/post/180469/
-		          location.href.substring(0, location.href.lastIndexOf("/") + 1) +
-		          "&aq=f&sourceid=chrome&ie=UTF-8" });
+	var aTag = document.createElement('a');
+	aTag.setAttribute('href',"http://webcache.googleusercontent.com/search?q=cache%3A" +
+						// http://habrahabr.ru/post/180469/?utm_source=feedly -> http://habrahabr.ru/post/180469/
+						location.href.substring(0, location.href.lastIndexOf("/") + 1) +
+						"&aq=f&sourceid=chrome&ie=UTF-8");
+	aTag.innerHTML = "Просмотреть в кеше Google";
+	h1.appendChild(aTag);
 }
